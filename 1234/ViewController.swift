ef771401a102890f588cc824b2476eb57bd6cee9
messierr <>
2022-01-19 08:48:12 +0300

    Initial Commit

---
+++ 1234/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1234
+//
+//  Created by 35 кабинет on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
